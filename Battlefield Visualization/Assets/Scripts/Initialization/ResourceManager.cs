84de166cc0e9030fef359787840cd7ccbd936607
Gal Kos <15galy@gmail.com>
2017-03-27 03:16:31 +0200

    no-issue ADD ResourceManager implementation and IniFile reader

---
+++ Battlefield Visualization/Assets/Scripts/Initialization/ResourceManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceManager : MonoBehaviour {
+
+	//contains data that is to be passed
+	public string blueFilePath;
+	public string neutralFilePath;
+	public string redFilePath;
+
+	void Awake() 
+	{
+		// stops object from automatically destroyed on loading a scene
+		DontDestroyOnLoad (this);
+	}
+
+}
